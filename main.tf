92ba080825bcaa4de237d010a2a21473d20f6e24
Ashwini Kanagaraj <153802512+AshwiniDevOpz@users.noreply.github.com>
2025-04-28 11:49:02 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  force_destroy = true
+
+  # (your other settings)
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default = "mybuckertss"
+}
+
+variable "aws_region" {
+  description ="AWS Region to create the bucket"
+  type        = string
+  default     = "us-east-2"
+}
