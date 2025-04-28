4f18c4c4831f344aafb0537429f91fe3db793fdb
Ashwini Kanagaraj <153802512+AshwiniDevOpz@users.noreply.github.com>
2025-04-28 12:47:24 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "example" {
-  bucket        = var.bucket_name
-  force_destroy = true
+  bucket                  = var.bucket_name
+  force_destroy           = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 
   # (your other settings)
 }
