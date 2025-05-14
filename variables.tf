9a019a0a5165850f61304c9e5022c3ed61036610
Ashwini Kanagaraj <153802512+AshwiniDevOpz@users.noreply.github.com>
2025-05-14 16:32:52 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+  default     = "digidense-learning-platform"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-c"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-gke-cluster"
+}
+
+
